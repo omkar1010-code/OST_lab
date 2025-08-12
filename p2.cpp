9598114fb757a8955504f08c366e6c19abf28d60
OmkarK <orkarande_b24@it.vjti.ac.in>
2025-08-12 14:55:29 +0530

    Update p2.cpp

---
+++ p2.cpp
@@ -0,0 +1,17 @@
+// omkar ramesh karande
+// 241080031
+#include<iostream>
+using namespace std;
+
+
+int main(){
+int arr[]={31,28,31,30,31,30,31,31,30,31,30,31};
+int x;
+cout<<"enter the month number"<<endl;
+cin>>x;
+
+cout<<"the number of days in the given month number is :"<<arr[x-1]<<endl;
+
+return 0;
+
+}
